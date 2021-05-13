433110f4802265dfd9b4be21a99fc5182eabed98
Tomasz Lorenc <lorenc.tomasz@gmail.com>
2021-05-13 12:11:12 +0200

    chore: make maskedCorners and cornerRadius props optional

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,8 @@ type SimpleGradientProgressbarViewProps = {
   fromColor: string; // color 
   toColor: string; // color
   progress: number; // 0.0 - 1.0
-  maskedCorners: Array<number>; 
-  cornerRadius: number;
+  maskedCorners?: Array<number>; 
+  cornerRadius?: number;
 };
 
 export const SimpleGradientProgressbarViewViewManager = requireNativeComponent<SimpleGradientProgressbarViewProps>(
